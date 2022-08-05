0f240bcb923857f6a40ffb9124e402e7e94511fa
Svyatoslav Andreev <sviatoslav-a@yandex.ru>
2022-08-05 10:59:18 +0300

    Added main() and folder creation

--- 2chScraper/main.py
+++ 2chScraper/main.py
@@ -1,33 +1,40 @@
+from os import makedirs
+from os.path import exists
+
 from requests import get
 from json import loads
 from wget import download
+if __name__ == '__main__':
+    def main():
+        print("Enter the thread url: ")
+        url = input()
 
-print("Enter the thread url: ")
-url = input()
+        i = -1
+        j = -1
+        folder_path = "C:\\Users\\Свят\\Desktop\\pics"
 
-i = -1
-j = -1
-folderPath = "C:\\Users\\Свят\\Desktop\\pics"
+        if ".html" in url:
+            url = url.replace(".html", ".json")
+        delimiter = "hk"
+        parts = url.split(delimiter)
+        base_url = parts[0] + delimiter
 
-if ".html" in url:
-    url = url.replace(".html", ".json")
-delimiter = "hk"
-parts = url.split(delimiter)
-base_url = parts[0] + delimiter
+        scraper = get(url)
+        result = scraper.content
+        js = loads(result)
 
-scraper = get(url)
-result = scraper.content
-js = loads(result)
+        if not exists(folder_path):
+            makedirs(folder_path)
 
-for posts in js['threads'][0]['posts']:
-    i += 1
-    for files in js['threads'][0]['posts'][i]['files']:
-        j += 1
-        ref = js['threads'][0]['posts'][i]['files'][j]['path']
-        if ".html" not in ref and ".mp4" not in ref:
-            imgUrl = ref
-            print(imgUrl)
-            download(base_url+imgUrl, folderPath)
-    j = -1
-i = -1
-print("Success")
+        for _ in js['threads'][0]['posts']:
+            i += 1
+            for _ in js['threads'][0]['posts'][i]['files']:
+                j += 1
+                ref = js['threads'][0]['posts'][i]['files'][j]['path']
+                if ".html" not in ref and ".mp4" not in ref:
+                    img_url = ref
+                    print(img_url)
+                    download(base_url+img_url, folder_path)
+            j = -1
+        print("Success")
+main()
